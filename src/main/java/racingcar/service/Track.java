9f7654cc40f94636cd656ff0f5e77cdbf0b8a77b
K-KY <kyuyoungk@naver.com>
2025-10-23 07:23:45 +0900

    feat : 자동차 객체를 관리할 일급 컬랙션
    - 새로운 자동차 객체 추가
    - 참가 자동차 개수 반환
    - 중복된 자동차 확인
    - 수정 불가능한 자동차 리스트 반환
    - 자동차들의 진행도 리스트 반환
    - 자동차들의 진행도 업데이트

---
+++ src/main/java/racingcar/service/Track.java
@@ -0,0 +1,44 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Track {
+    private final List<Car> cars;
+
+    public Track() {
+        this.cars = new ArrayList<>();
+    }
+
+    public boolean addCar(Car car) {
+        if (contains(car)) {
+            return false;
+        }
+        cars.add(car);
+        return true;
+    }
+
+    public int participantsCount() {
+        return cars.size();
+    }
+
+    public boolean contains(Car car) {
+        return cars.contains(car);
+    }
+
+    public List<Car> getCars() {
+        return Collections.unmodifiableList(cars);
+    }
+
+    public List<String> getParticipantProgress() {
+        return cars.stream()
+                .map(Car::toString)
+                .toList();
+    }
+
+    public List<String> rollProgress() {
+        cars.forEach(car -> car.updateProgress(RandomNumberGenerator.getRandomNumber()));
+        return getParticipantProgress();
+    }
+}
