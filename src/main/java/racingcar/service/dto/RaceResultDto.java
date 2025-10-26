762520b2075d3a51e5d033a81d55f7b911cf6721
K-KY <kyuyoungk@naver.com>
2025-10-26 16:23:21 +0900

    feat : 컨트롤러에서 반환할 RaceResultDto
    - 레이스 결과와 우승자를 반환

---
+++ src/main/java/racingcar/service/dto/RaceResultDto.java
@@ -0,0 +1,8 @@
+package racingcar.service.dto;
+
+import racingcar.service.Car;
+import racingcar.service.RaceLog;
+
+import java.util.List;
+
+public record RaceResultDto(List<RaceLog> raceLogs, List<Car> winners) {}
