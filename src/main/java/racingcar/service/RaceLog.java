3628a9229c28e6aae90935a12b1c3a5028b2d2a1
K-KY <kyuyoungk@naver.com>
2025-10-26 16:28:06 +0900

    feat : `RaceLog`에 toString 메서드 추가
    - toString 형식
    ```
    123 :
    234 : -
    ```

--- src/main/java/racingcar/service/RaceLog.java
+++ src/main/java/racingcar/service/RaceLog.java
@@ -2,6 +2,7 @@ package racingcar.service;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class RaceLog {
     private final List<String> log;
@@ -12,5 +13,12 @@ public class RaceLog {
 
     public List<String> getLog() {
         return Collections.unmodifiableList(log);
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner("\n");
+        log.forEach(joiner::add);
+        return joiner.toString();
     }
 }
